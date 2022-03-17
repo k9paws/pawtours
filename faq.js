2ff771d33590fd2783a3a282667f4c6c7f665845
k9paws <candogservices@gmail.com>
2022-03-17 17:22:49 -0400

    added faq section with filler content

---
+++ faq.js
@@ -0,0 +1,8 @@
+// FAQ Questions Drop Down
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
